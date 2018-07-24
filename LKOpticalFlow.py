2b2e2b66f2e2302d6de855c98c31479b1ff7e279
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2018-07-24 18:09:38 +1200

    LK optical flow as a separate class.

---
+++ LKOpticalFlow.py
@@ -0,0 +1,17 @@
+import cv2
+
+
+class LKOpticalFlow:
+    def __init__(self, win=(20, 20), max=2):
+        self.lk_params = dict(winSize=win, maxLevel=max,
+                              criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+        self.feature_params = dict(maxCorners=100,
+                                   qualityLevel=0.3,
+                                   minDistance=7,
+                                   blockSize=7)
+
+    def lk(self, curr_im, nxt_im, p0):
+        p1, st, err = cv2.calcOpticalFlowPyrLK(curr_im, nxt_im, p0, None, **self.lk_params)
+
+        return p1, st, err
