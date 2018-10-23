3b92f23b4386b2749e4f5f8f1aa2a3a44f75a9e7
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2018-10-24 11:37:22 +1300

    testing OF Farneback for segmentation.

--- src/sparc/videotracking/processing.py
+++ src/sparc/videotracking/processing.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
-
-
+import os
 # from matplotlib import pyplot as plt
 
 
@@ -177,3 +176,47 @@ class Processing:
         cv2.imshow('Ventricle Edge', blend)
         cv2.waitKey(0)
         return None
+
+
+if __name__ == '__main__':
+    path = r'/hpc/mosa004/Sparc/Heart/ImagesSmallSample'
+    animation = True
+    count = 1
+    while animation:
+        for i in sorted(os.listdir(path)):
+            if count == 1:
+                file_name = i
+                PS = Processing()
+                imfile = os.path.join(path, file_name)
+                PS.read_image(imfile)
+                gray = PS.rgb_and_blur_and_hsv(threshold=9)
+                hsv = np.zeros_like(gray)
+                hsv[..., 1] = 255
+            else:
+                if PS is not None: del PS
+                file_name = 'HeartVideo0015.jpg'
+                PS = Processing()
+                next_imfile = os.path.join(path, file_name)
+                PS.read_image(next_imfile)
+                next_gray = PS.rgb_and_blur_and_hsv(threshold=9)
+
+                flow = cv2.calcOpticalFlowFarneback(gray, next_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+                mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+
+                for i in range(mag.shape[0]):
+                    for j in range(mag.shape[1]):
+                        if mag[i][j] < 2:
+                            gray[i][j] = 0
+                hsv[..., 0] = ang * 180 / np.pi / 2
+                hsv[..., 2] = cv2.normalize(mag, None, 0, 0, cv2.NORM_MINMAX)
+                bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+                cv2.imshow('frame2', bgr)
+                gray = next_gray
+
+            k = cv2.waitKey(20) & 0xFF
+            count += 25
+            print("Frame : %s" % i)
+            if k == 27:
+                animation = False
+                break
